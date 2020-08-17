de6be1cceb6688c17b1838defe565b5aab24ad2f
Kyle Schneider <kyleschneider@Kyles-MacBook-Air.local>
2020-08-17 15:14:54 -0700

    all in

---
+++ src/api/quizQuestions.js
@@ -0,0 +1,256 @@
+var quizQuestions = [
+  {
+      question: "Can you walk, sit and stand for 60 minutes with no pain or fatigue?",
+      answers: [
+          {
+              type: "no Physical Therapy",
+              content: "Yes"
+          },
+          {
+              type: "Physical Therapy",
+              content: "No"
+          }
+      ]
+  },
+  {
+      question: "Can you sleep un-interrupted for 7-8 hours?",
+      answers: [
+        {
+            type: "no Physical Therapy",
+            content: "Yes"
+        },
+        {
+            type: "Physical Therapy",
+            content: "No"
+        }
+    ]
+  },
+  {
+      question: "Can you do your daily activities like showering, washing your hair, dressing, putting on socks and shoes independently with no pain or fatigue?",
+      answers: [
+        {
+            type: "no Physical Therapy",
+            content: "Yes"
+        },
+        {
+            type: "Physical Therapy",
+            content: "No"
+        }
+    ]
+  },
+  {
+      question: "Can you go drive to the market for groceries and pick up your own medicine?",
+      answers: [
+        {
+            type: "no Physical Therapy",
+            content: "Yes"
+        },
+        {
+            type: "Physical Therapy",
+            content: "No"
+        }
+    ]
+  },
+  {
+      question: "Can you void and defecate with no issues?",
+      answers: [
+        {
+            type: "no Physical Therapy",
+            content: "Yes"
+        },
+        {
+            type: "Physical Therapy",
+            content: "No"
+        }
+    ]
+  },
+  {
+    question: "Can you swallow, chew, talk and sing without any problems?",
+    answers: [
+        {
+            type: "no Physical Therapy",
+            content: "Yes"
+        },
+        {
+            type: "Physical Therapy",
+            content: "No"
+        }
+    ]
+    },
+    {
+        question: "Can you squat to the floor without losing your balance 5-6 times without pain?",
+        answers: [
+            {
+                type: "no Physical Therapy",
+                content: "Yes"
+            },
+            {
+                type: "Physical Therapy",
+                content: "No"
+            }
+        ]
+    },
+    {
+        question: "Can you squat to the floor without losing your balance 5-6 times without pain?",
+        answers: [
+            {
+                type: "no Physical Therapy",
+                content: "Yes"
+            },
+            {
+                type: "Physical Therapy",
+                content: "No"
+            }
+        ]
+    },
+    {
+        question: "Could you get up off the floor if you fell down with no assistance?",
+        answers: [
+            {
+                type: "no Physical Therapy",
+                content: "Yes"
+            },
+            {
+                type: "Physical Therapy",
+                content: "No"
+            }
+        ]
+    },
+    {
+        question: "Can you write, type, use the phone with ease?",
+        answers: [
+            {
+                type: "no Physical Therapy",
+                content: "Yes"
+            },
+            {
+                type: "Physical Therapy",
+                content: "No"
+            }
+        ]
+    },
+    {
+        question: "Do you have heaviness, pressure or dullness in you arms, hands, legs, feet or abdomen?",
+        answers: [
+            {
+                type: "Physical Therapy",
+                content: "Yes"
+            },
+            {
+                type: "no Physical Therapy",
+                content: "No"
+            }
+        ]
+    },
+    {
+        question: "Do you have numbness, tingling or pins and needles in your hands, feet or other parts of your body?",
+        answers: [
+            {
+                type: "Physical Therapy",
+                content: "Yes"
+            },
+            {
+                type: "no Physical Therapy",
+                content: "No"
+            }
+        ]
+    },
+    {
+        question: "Do you wake in the morning with pain or stiffness in your neck, back, legs or arms?",
+        answers: [
+            {
+                type: "Physical Therapy",
+                content: "Yes"
+            },
+            {
+                type: "no Physical Therapy",
+                content: "No"
+            }
+        ]
+    },
+    {
+        question: "Have you fallen in the past 6 months?",
+        answers: [
+            {
+                type: "Physical Therapy",
+                content: "Yes"
+            },
+            {
+                type: "no Physical Therapy",
+                content: "No"
+            }
+        ]
+    },
+    {
+        question: "Do you have swelling in your arms, hands, legs, feet or abdomen?",
+        answers: [
+            {
+                type: "Physical Therapy",
+                content: "Yes"
+            },
+            {
+                type: "no Physical Therapy",
+                content: "No"
+            }
+        ]
+    },
+    {
+        question: "Do you feel like it’s hard to move your arms, legs or body?",
+        answers: [
+            {
+                type: "Physical Therapy",
+                content: "Yes"
+            },
+            {
+                type: "no Physical Therapy",
+                content: "No"
+            }
+        ]
+    },
+    {
+        question: "Do you have scar tissue that pulls, tugs or makes you feel like you can’t move?",
+        answers: [
+            {
+                type: "Physical Therapy",
+                content: "Yes"
+            },
+            {
+                type: "no Physical Therapy",
+                content: "No"
+            }
+        ]
+    },
+    {
+        question: "Have you had any radiated tissue?",
+        answers: [
+            {
+                type: "Physical Therapy",
+                content: "Yes"
+            },
+            {
+                type: "no Physical Therapy",
+                content: "No"
+            }
+        ]
+    },
+    {
+        question: "Are you currently in active treatment for cancer?",
+        answers: [
+            {
+                type: "no Physical Therapy",
+                content: "Yes"
+            },
+            {
+                type: "Physical Therapy",
+                content: "No"
+            }
+        ]
+    }
+
+
+
+
+  
+];
+
+export default quizQuestions;
